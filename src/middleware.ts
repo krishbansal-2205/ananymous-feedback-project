301d26cca7e6a25eaf8fa9654b3abb7f2d07840e
Krish Bansal <krishbansalkssk@outlook.com>
2025-02-19 19:35:15 +0530

    Implement authentication with NextAuth, including sign-in page, middleware, and user session management

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+// export { default } from 'next-auth/middleware';
+
+export const config = {
+    matcher: ['/dashboard/:path*', '/signin', '/signup', '/', '/verify/:path*'],
+};
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
+
+    // Redirect to dashboard if the user is already authenticated
+    // and trying to access sign-in, sign-up, or home page
+    if (
+        token &&
+        (url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname === '/')
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
+    return NextResponse.next();
+}
+// import { auth } from "@/auth"
+// import { getToken } from "next-auth/jwt";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export default auth(async (req:NextRequest) => {
+//     const token=await getToken({req});
+//     const url=req.nextUrl;
+//     if(token && (url.pathname.startsWith('/signin') || url.pathname.startsWith('/signup') || url.pathname.startsWith('/verify'))){
+//         return NextResponse.redirect(new URL('/dashboard',req.url))
+//     }
+
+//     return NextResponse.redirect(new URL('/',req.url))
+// })
+
+// export const config = {
+//   matcher: ['/', '/signin','/dashboard'],
+// }
